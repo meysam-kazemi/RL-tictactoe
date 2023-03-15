e0aa16474fcd8c7bf0a9ccf371187b668dc8a9de
meysam-kazemi <meisam.daysu@gmail.com>
2023-03-16 02:26:38 +0330

    create simple table

---
+++ tic_tac_toe.jl
@@ -0,0 +1,20 @@
+function center(x::string,width::Int)
+    w = width÷2
+    return " "^w * x * " "^w
+end
+function print_table(x)
+    x*2
+end
+
+
+# println(" "^5,"|"," "^5,"|"," "^5)
+# println("-"^17)
+# println(" "^5,"|"," "^5,"|"," "^5)
+# println("-"^17)
+# println(" "^5,"|"," "^5,"|"," "^5)
+
+tabel = [" " " " " ";" " " " " ";" " " " " "]
+
+
+
+
