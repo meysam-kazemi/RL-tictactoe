2fc0e647c664fe982c52106ca39ff568f3144480
meysam-kazemi <meisam.daysu@gmail.com>
2023-03-16 03:23:18 +0330

    complete functions

--- tic_tac_toe.jl
+++ tic_tac_toe.jl
@@ -1,11 +1,11 @@
-function center(x::String,width::Int)
-    w = width÷2
+function center(x::String)
+    w = 3
     return " "^w * x * " "^w
 end
 function table_builder(table)
     text = ""
     for i in 1:3
-        text *= center(table[i,1],6)*"|"*center(table[i,2],6)*"|"*center(table[i,3],6)*"\n"
+        text *= center(table[i,1])*"|"*center(table[i,2])*"|"*center(table[i,3])*"\n"
         if i !=3; text *= "-"^22*"\n"; end
     end
     return text
@@ -17,11 +17,13 @@ function update(loc,who)
     global table
     who in ["circle","square"] ? nothing : error("who -> circle or square ")
     marker = who == "circle" ? "◌" : "◻"
-    table[loc] = center(marker,6);
+    table[loc] = marker;
 end
 update(2,"circle")
 print(table_builder(table))
 
+update(3,"square")
+print(table_builder(table))
 
 
 
