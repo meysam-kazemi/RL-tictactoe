d7fa1f1c216435dcf7999b1ba9be36a32254a9e9
meysam-kazemi <meisam.daysu@gmail.com>
2023-03-16 03:16:22 +0330

    create update function

--- tic_tac_toe.jl
+++ tic_tac_toe.jl
@@ -1,19 +1,41 @@
-function center(x::string,width::Int)
+function center(x::String,width::Int)
     w = width÷2
     return " "^w * x * " "^w
 end
-function print_table(x)
-    x*2
+function table_builder(table)
+    text = ""
+    for i in 1:3
+        text *= center(table[i,1],6)*"|"*center(table[i,2],6)*"|"*center(table[i,3],6)*"\n"
+        if i !=3; text *= "-"^22*"\n"; end
+    end
+    return text
 end
+table = [" " " " " ";" " " " " ";" " " " " "]
+# print(table_builder(table))
+
+function update(loc,who)
+    global table
+    who in ["circle","square"] ? nothing : error("who -> circle or square ")
+    marker = who == "circle" ? "◌" : "◻"
+    table[loc] = center(marker,6);
+end
+update(2,"circle")
+print(table_builder(table))
 
 
-# println(" "^5,"|"," "^5,"|"," "^5)
-# println("-"^17)
-# println(" "^5,"|"," "^5,"|"," "^5)
-# println("-"^17)
-# println(" "^5,"|"," "^5,"|"," "^5)
 
-tabel = [" " " " " ";" " " " " ";" " " " " "]
+
+
+
+
+
+
+
+
+
+
+
+println("="^40)
 
 
 
