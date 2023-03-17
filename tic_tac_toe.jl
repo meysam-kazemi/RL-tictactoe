1fe263681515d1f29a300732a2ceed1608ddefe1
meysam-kazemi <meisam.daysu@gmail.com>
2023-03-17 04:48:01 +0330

    complete check_win function

--- tic_tac_toe.jl
+++ tic_tac_toe.jl
@@ -2,6 +2,7 @@ function center(x::String)
     w = 3
     return " "^w * x * " "^w
 end
+
 function table_builder(table)
     text = ""
     for i in 1:3
@@ -10,12 +11,35 @@ function table_builder(table)
     end
     return text
 end
+
 function update(loc,who)
     global table
     who in ["circle","square"] ? nothing : error("who -> circle or square ")
     marker = who == "circle" ? "◌" : "◻"
     table[loc] = marker;
 end
+
+function check_win()
+    global table
+    win = false
+    for i in 1:3
+        if table[1,i]==table[2,i] && table[1,i]==table[3,i]
+            win = true
+        elseif table[i,1]==table[i,2] && table[i,1]==table[i,3]
+            win = true
+        end
+    end
+    if table[1,1]==table[2,2] && table[1,1]==table[3,3]
+        win = true
+    elseif table[3,1]==table[2,2] && table[3,1]==table[1,3]
+        win = true
+    end
+    return win
+end
+
+
+Base.run(`clear`) # Clear the output
+println("="^40)
 table = ["1" "4" "7";"2" "5" "8";"3" "6" "9"]
 print(table_builder(table))
 
@@ -28,23 +52,7 @@ for i in 1:8
     i%2 != 0 ? push!(order,who_is_first) : push!(order,ci_sq[ci_sq.!=who_is_first][1])
 end
 
-function check_win()
-    global table
-    win = false
-    for i in 1:3
-        if all(i->i==table[i,1],table[i,:]) # Check if all elements are equal or not
-            win = true 
-        elseif all(i->i==table[1,i],table[:,i])
-            win = true
-        end
-    end
-    if table[1,1]==table[2,2] && table[1,1]==table[3,3]
-        win = true
-    elseif table[3,1]==table[2,2] && table[3,1]==table[1,3]
-        win = true
-    end
-    return win
-end
+
 
 unchanged = ones(Bool,9)
 for who in order
@@ -53,6 +61,7 @@ for who in order
     loc = parse(Int8,readline())
     update(loc,who)
     Base.run(`clear`) # Clear the output
+    println("="^40)
     print(table_builder(table))
     unchanged[loc] = 0
     if check_win()
@@ -60,41 +69,18 @@ for who in order
         break
     end
 end
-update(findall(x->x.==1,unchanged)[1],who_is_first)
-Base.run(`clear`)
-print(table_builder(table))
-check_win()
-if check_win()
-    println("$who WIN !")
+if !check_win()
+    update(findall(x->x.==1,unchanged)[1],who_is_first)
+    Base.run(`clear`)
+    println("="^40)
+    print(table_builder(table))
+    check_win()
+    if check_win()
+        println("--$who_is_first WIN!--")
+    else
+        println("--EQUAL!--")
+    end
 end
-
-
-
-
-
-
-
-
-
-
-    
-
-    
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
 println("="^40)
 
 
